59b68c0a3a875787f7e4bea41d42735f18dfdf1c
vardan00 <lella.vardan@gmail.com>
2018-08-26 20:47:15 -0400

    added paddle two

--- start.py
+++ start.py
@@ -17,17 +17,22 @@ clock = pygame.time.Clock()
 
 
 paddle_one = Bar(300, 300)
+paddle_two = Bar(300, 300)
 
 c = Ball(screen_width, screen_height)
 
+keyDict = {"up":False, "down":False, "w":False, "s":False}
 
 def gameLoop():
     x = screen_height *.5
     y = screen_width *.5
+    y2 = screen_width *.5
 
     crashed = False
     close = False
-    y_change = 0    
+    y_change = 0
+    y_change2 = 0
+    
     while not crashed and not close:   
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -35,31 +40,61 @@ def gameLoop():
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
-                    y_change = -5
-                
+                    keyDict["up"] = True
                 elif event.key == pygame.K_DOWN:
-                    y_change = 5
-                
+                    keyDict["down"] = True
+                elif event.key == pygame.K_w:
+                    keyDict["w"] = True
+                elif event.key == pygame.K_s:
+                    keyDict["s"] = True
+                    
             if event.type == pygame.KEYUP:
-                if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                    y_change = 0
+                if event.key == pygame.K_UP:
+                    keyDict["up"] = False
+                elif event.key == pygame.K_DOWN:
+                    keyDict["down"] = False
+                elif event.key == pygame.K_w:
+                    keyDict["w"] = False
+                elif event.key == pygame.K_s:
+                    keyDict["s"] = False
 
+        if keyDict["up"]:
+            print(keyDict["up"])
+            y_change = -5
+        elif keyDict["down"]:
+            y_change = 5
+        elif not keyDict["down"] and not keyDict["up"]:
+            y_change = 0
+
+        if keyDict["w"]:
+            y_change2 = -5
+        elif keyDict["s"]:
+            print(keyDict['w'])
+            y_change2 = 5
+        elif not keyDict["s"] and not keyDict["s"]:
+            print(keyDict['w'])
+            y_change2 = 0
+
+    
         if y+y_change > 0 and y+y_change < screen_height-70:
             y+=y_change
-
+            
+        if y2+y_change2 > 0 and y2+y_change2 < screen_height-70:
+            y2+=y_change2
 
         gameDisplay.fill((0,0,0))
         render.drawBar(paddle_one, gameDisplay, 20, y, (255,255,255))
-
+        render.drawBar(paddle_two, gameDisplay, 580, y2, (255,255,255))
+        
         if c.x_left_boundary <= paddle_one.x_right_boundary:
             if (paddle_one.y_up_boundary <= c.y_top_boundary and paddle_one.y_bottom_boundary >= c.y_bottom_boundary):
                 render.drawCircle(c, gameDisplay, 400+c.x_velocity, 300+c.y_velocity, (255,255,255), True)
-
             else:
                 render.drawCircle(c, gameDisplay, 400+c.x_velocity, 300+c.y_velocity, (255,255,255))
-
         else:
             render.drawCircle(c, gameDisplay, 400+c.x_velocity, 300+c.y_velocity, (255,255,255))
+
+                
         pygame.display.update()
     
         clock.tick(60)
